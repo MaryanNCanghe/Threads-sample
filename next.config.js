1f12fda3e5b72742fe56fb57175d6c1a55943cca
Kiki <marianecanhe@gmail.com>
2024-09-17 18:13:39 +0200

    swc

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: false,
   experimental: {
     serverActions: true,
-    swcMinify: false,
+    
     serverComponentsExternalPackages: ["mongoose"],
   },
   eslint: {
